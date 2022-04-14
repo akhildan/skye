e30b20986e18a8bdaf177cf84d4a1831a39de02c
akhildan <akhil.dandamudi@outlook.com>
2022-04-14 13:16:07 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 import '../Style.css'
 import CitySearch from './CitySearch';
 import WeatherCard from './WeatherCard'
+const API_KEY = process.env.REACT_APP_API_KEY 
 
 class App extends React.Component{
 
     state = {weatherResult: null}
 
     onSearchSubmit = async (searchInputValue) => {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=889d9531a3375c443aa553b8bd543b4c`)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${API_KEY}`)
         this.setState({weatherResult: response.data})
      }
-
     render() {
         return(
             <div className="container p-4">
